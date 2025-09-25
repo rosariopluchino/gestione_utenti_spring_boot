5ed854b6b56f1e9334cbb03370dcd804f9442c0e
rosario.pluchino <rosario.pluchino@aitho.it>
2025-09-25 16:47:03 +0200

    chore: rimosso file non necessario

--- src/test/java/gestione/utenti/gestione/utenti/ApplicationTests.java
+++ src/test/java/gestione/utenti/gestione/utenti/ApplicationTests.java
@@ -1,4 +1,4 @@
-package gestione.utenti.gestione.utenti;
+/*package gestione.utenti.gestione.utenti;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class ApplicationTests {
 	}
 
 }
+*/
